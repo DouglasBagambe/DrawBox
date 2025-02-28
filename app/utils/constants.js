d444bcbdac232b886cab813ac2ea85080a8c700b
douglasbagambe <douglasbagambe4@gmail.com>
2025-02-28 18:05:09 +0300

    Initial

---
+++ app/utils/constants.js
@@ -0,0 +1,9 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MASTER_SEED = "master";
+export const LOTTERY_SEED = "lottery";
+export const TICKET_SEED = "ticket";
+
+export const PROGRAM_ID = new PublicKey(
+  "3yRy8B1E1pd5FxpuGe72s6tjEAD6v52hq81Kb2U3dpCg"
+);
