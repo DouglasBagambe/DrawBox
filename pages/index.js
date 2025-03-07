0376009b5aeed8d404f0e53427667389a1def64b
douglasbagambe <douglasbagambe4@gmail.com>
2025-03-07 15:34:54 +0300

    hardhat

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,32 @@
 // pages/index.js
 
 import { useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-
-require("@solana/wallet-adapter-react-ui/styles.css");
-
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { mainnet, localhost } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import LotteryDapp from "../components/LotteryDapp";
-import Header from "../components/Header";
-// import { AppProvider } from "../context/context";
-// import { SolanaProvider } from "../context/SolanaContext";
 import { AppProvider } from "../context/context";
 
-export default function Home() {
-  const endpoint = clusterApiUrl(WalletAdapterNetwork.Devnet);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+const { chains, publicClient } = configureChains(
+  [localhost], // Switch to Sepolia/mainnet later
+  [publicProvider()]
+);
 
+const config = createConfig({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  publicClient,
+});
+
+export default function Home() {
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <AppProvider>
-            <div className="relative min-h-screen">
-              <LotteryDapp />
-            </div>
-          </AppProvider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <WagmiConfig config={config}>
+      <AppProvider>
+        <div className="relative min-h-screen">
+          <LotteryDapp />
+        </div>
+      </AppProvider>
+    </WagmiConfig>
   );
 }
