d599cc830860a62d1cdefd3f4d6527a70683c602
douglasbagambe <douglasbagambe4@gmail.com>
2025-03-07 15:33:20 +0300

    hardhat

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,6 @@
 // utils/constants.js
 
-import { PublicKey } from "@solana/web3.js";
-
-export const MASTER_SEED = "master";
-export const LOTTERY_SEED = "lottery";
-export const TICKET_SEED = "ticket";
-
-export const PROGRAM_ID = new PublicKey(
-  "3yRy8B1E1pd5FxpuGe72s6tjEAD6v52hq81Kb2U3dpCg"
-);
+export const LOTTERY_CONTRACT_ADDRESS =
+  "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // From your local deploy, update later
+export const LOTTERY_ABI = require("./LotterySystemABI.json"); // ABI from Hardhat
+export const TICKET_PRICE = "0.01"; // Ether, matches your test
