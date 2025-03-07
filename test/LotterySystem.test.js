c0a03863f3b9480718f0ab36ae5449f8ca6ea0e7
douglasbagambe <douglasbagambe4@gmail.com>
2025-03-07 15:14:34 +0300

    hardhat

--- test/LotterySystem.test.js
+++ test/LotterySystem.test.js
@@ -2,28 +2,30 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("LotterySystem", function () {
-  let LotterySystem, lottery, owner, addr1;
+  let LotterySystem, lottery, owner, addr1, addr2;
 
   beforeEach(async function () {
-    [owner, addr1] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
     LotterySystem = await ethers.getContractFactory("LotterySystem");
     lottery = await LotterySystem.deploy();
     await lottery.deployed();
+    await lottery.assignRole(addr1.address, "authority");
+    await lottery.assignRole(addr2.address, "participant");
   });
 
   it("Should create a lottery", async function () {
-    await lottery.createLottery(ethers.utils.parseEther("0.01"));
+    await lottery.connect(addr1).createLottery(ethers.utils.parseEther("0.01"));
     const lotteryInfo = await lottery.getLotteryInfo(1);
     expect(lotteryInfo.id.toString()).to.equal("1");
-    expect(lotteryInfo.authority).to.equal(owner.address);
+    expect(lotteryInfo.authority).to.equal(addr1.address);
   });
 
   it("Should buy a ticket", async function () {
-    await lottery.createLottery(ethers.utils.parseEther("0.01"));
+    await lottery.connect(addr1).createLottery(ethers.utils.parseEther("0.01"));
     await lottery
-      .connect(addr1)
+      .connect(addr2)
       .buyTicket(1, { value: ethers.utils.parseEther("0.01") });
-    const [id, lotteryId, ticketOwner] = await lottery.getTicketInfo(1, 1); // Destructure tuple
-    expect(ticketOwner).to.equal(addr1.address);
+    const [id, lotteryId, ticketOwner] = await lottery.getTicketInfo(1, 1);
+    expect(ticketOwner).to.equal(addr2.address);
   });
 });
