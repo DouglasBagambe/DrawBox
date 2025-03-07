a424c0a729cb1cc4fc5b5c4fe378cc2b15d51cb9
douglasbagambe <douglasbagambe4@gmail.com>
2025-03-07 15:34:28 +0300

    hardhat

--- utils/contract.js
+++ utils/contract.js
@@ -1,57 +1,40 @@
 // utils/program.js
 
-import { AnchorProvider, BN, Program } from "@project-serum/anchor";
-import { PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { ethers } from "ethers";
+import { LOTTERY_CONTRACT_ADDRESS, LOTTERY_ABI } from "./constants";
 
-import IDL from "./idl.json";
-import {
-  LOTTERY_SEED,
-  MASTER_SEED,
-  PROGRAM_ID,
-  TICKET_SEED,
-} from "./constants";
-
-// How to fetch our Program
-export const getProgram = (connection, wallet) => {
-  const provider = new AnchorProvider(connection, wallet, {
-    commitment: "confirmed",
-  });
-  const program = new Program(IDL, PROGRAM_ID, provider);
-  return program;
+export const getContract = (providerOrSigner) => {
+  return new ethers.Contract(
+    LOTTERY_CONTRACT_ADDRESS,
+    LOTTERY_ABI,
+    providerOrSigner
+  );
 };
 
-export const getMasterAddress = async () => {
-  return (
-    await PublicKey.findProgramAddress([Buffer.from(MASTER_SEED)], PROGRAM_ID)
-  )[0];
+export const getLotteryInfo = async (provider, lotteryId) => {
+  const contract = getContract(provider);
+  const info = await contract.getLotteryInfo(lotteryId);
+  return {
+    id: info.id.toString(),
+    authority: info.authority,
+    ticketPrice: info.ticketPrice.toString(),
+    lastTicketId: info.lastTicketId.toString(),
+    winnerId: info.winnerId.toString(),
+    winnerChosen: info.winnerChosen,
+    claimed: info.claimed,
+    totalPrize: info.totalPrize.toString(),
+  };
 };
 
-export const getLotteryAddress = async (id) => {
-  return (
-    await PublicKey.findProgramAddress(
-      [Buffer.from(LOTTERY_SEED), new BN(id).toArrayLike(Buffer, "le", 4)],
-      PROGRAM_ID
-    )
-  )[0];
+export const getTicketInfo = async (provider, lotteryId, ticketId) => {
+  const contract = getContract(provider);
+  const [id, lottery, owner] = await contract.getTicketInfo(
+    lotteryId,
+    ticketId
+  );
+  return { id: id.toString(), lotteryId: lottery.toString(), owner };
 };
 
-export const getTicketAddress = async (lotteryPk, id) => {
-  return (
-    await PublicKey.findProgramAddress(
-      [
-        Buffer.from(TICKET_SEED),
-        lotteryPk.toBuffer(),
-        new BN(id).toArrayLike(Buffer, "le", 4),
-      ],
-      PROGRAM_ID
-    )
-  )[0];
-};
-
-// Return the lastTicket ID and multiply the ticket price and convert LAMPORTS PER SOL and convert it to String
 export const getTotalPrize = (lottery) => {
-  return new BN(lottery.lastTicketId)
-    .mul(lottery.ticketPrice)
-    .div(new BN(LAMPORTS_PER_SOL))
-    .toString();
+  return ethers.utils.formatEther(lottery.lastTicketId * lottery.ticketPrice);
 };
